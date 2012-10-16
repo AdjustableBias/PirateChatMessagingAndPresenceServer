37c1567da9145d858e3eef2023b769e30b57a667
andersbe <andersbe@gmail.com>
2012-10-16 09:46:35 -0700

    Initial commit

---
+++ WorkerRole1/server.js
@@ -0,0 +1,246 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+  , sio = require('socket.io');
+
+/**
+ * App.
+ */
+
+var app = express.createServer();
+
+/**
+ * App configuration.
+ */
+
+app.configure(function () {
+  app.use(stylus.middleware({ src: __dirname + '/public', compile: compile }));
+  app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname);
+  app.set('view engine', 'jade');
+
+  function compile (str, path) {
+    return stylus(str)
+      .set('filename', path)
+      .use(nib());
+  };
+});
+
+/**
+ * App routes.
+ */
+
+//app.get('/', function (req, res) {
+//  res.render('index', { layout: false });
+//});
+
+/**
+ * App listen.
+ */
+
+app.listen(process.env.port || 3000, function () {
+  var addr = app.address();
+  console.log('   app listening on http://' + addr.address + ':' + addr.port);
+});
+
+/**
+ * Socket.IO server (single process only)
+ */
+
+var io = sio.listen(app)
+
+io.configure(function () {
+    io.set("transports", [
+        "xhr-polling",
+        "jsonp-polling",
+        "htmlfile"
+    ]);
+    io.set("polling duration", 10);
+});
+
+// TODO: break this up into activeUsers and userSubscribers so you don't have to distinguish between a user
+// who isn't connected but is subscribed to by a connected user and one that is fully connected by weird socket checks
+var users = {};
+var addSocketToUser = function server$addSocketToUser (socketToAdd, user) {
+    var id = user.nextSocketId;
+    user.nextSocketId++;
+    if (!user.sockets) { user.sockets = {}; }
+    user.sockets[id] = socketToAdd;
+    return id;
+}
+
+var addSubscribedToNames = function server$addSubscribedToNames (names, user, socket) {
+    user.subscribedToNames = names;
+
+    for (var i = 0; i < names.length; i++) {
+        // First add ourself as a subscriber to the users defined by names
+        if (!users.hasOwnProperty(names[i])) {
+            users[names[i]] = { name: names[i], subscribers: {}, status: "offline" }
+        }
+        var subscribedTo = users[names[i]];
+        subscribedTo.subscribers[user.name] = user;
+        if (socket) {
+            console.log("Sending StatusChange packet due to an initial subscription.");
+            socket.send(JSON.stringify({ messageId: "StatusChange", user: subscribedTo.name, status: subscribedTo.status }));
+        }
+    }
+}
+
+var statusChange = function server$statusChange (newStatus, user) {
+    if (user.subscribers && user.status != newStatus) {
+        user.status = newStatus;
+
+        // Notify any subscribers
+        for (var subscriberName in user.subscribers) {
+            if (user.subscribers.hasOwnProperty(subscriberName)) {
+                var sockets = user.subscribers[subscriberName].sockets;
+
+                if (sockets) {
+                    for (var socketId in sockets) {
+                        if (sockets.hasOwnProperty(socketId) && sockets[socketId]) {
+                            sockets[socketId].send(JSON.stringify({ messageId: "StatusChange", user: user.name, status: newStatus }));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+var hasAnyOwnPropertyHelper = function server$hasAnyOwnPropertyHelper (obj) {
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop) && obj[prop] !== undefined) {
+            return true;
+        }
+    }
+    return false;
+}
+
+var removeUser = function server$removeUser(user) {
+    statusChange("offline", user);
+    // remove it as a subscriber of the users it's subscribed to
+    if (user.subscribedToNames) {
+        var names = user.subscribedToNames;
+        for (var i = 0; i < names; i++) {
+            var name = names[i];
+            if (users.hasOwnProperty(name) && users[name]) {
+                var user = users[name];
+                delete user.subscribers[user.name];
+            }
+        }
+    }
+
+    // check if it has any subscribers 
+    var hasSubscribers = hasAnyOwnPropertyHelper(user.subscribers);
+
+    if (!hasSubscribers) {
+        delete users[user.name];
+    } else {
+        user.subscribedToNames = undefined;
+        user.sockets = {};
+        user.status = "offline";
+    }
+}
+
+var removeSocket = function server$removeSocket (user, socketId) {
+    if (user.sockets && user.sockets.hasOwnProperty(socketId) && user.sockets[socketId]) {
+        delete user.sockets[socketId];
+    }
+
+    var hasAnySockets = hasAnyOwnPropertyHelper(user.sockets);
+
+    if (!hasAnySockets) {
+        removeUser(user);
+    }
+}
+
+var sendMessageToUser = function server$sendMessageToUser (toUser, fromUser, messageText) {
+    if (toUser.sockets) {
+        for (var socketId in toUser.sockets) {
+            if (toUser.sockets.hasOwnProperty(socketId) && toUser.sockets[socketId]) {
+                var messageObj = { messageId: "ChatMessage", fromUser: fromUser.name, messageText: messageText };
+                console.log("sending message: ");
+                console.dir(messageObj);
+                toUser.sockets[socketId].send(JSON.stringify(messageObj));
+            }
+        }
+    }
+}
+
+io.sockets.on('connection', function server$io$on$connection (socket) {
+    var user = undefined;
+    var socketId = 0;
+    socket.on('message', function server$io$on$message(message, ackCallback) {
+
+        var messageObj;
+        try {
+            messageObj = JSON.parse(message);
+        } catch (e) {
+            console.error("Problem parsing messageObj: " + e.description);
+        }
+
+        if (messageObj.messageId) {
+            switch (messageObj.messageId) {
+                case "Initialize":
+                    console.log("Received initialize packet with an obj of: ");
+                    console.dir(messageObj);
+                    // Only allow initialize if it hasn't been called
+                    if (socketId === 0) {
+                        // TODO: validate messageObj
+                        if (!users.hasOwnProperty(messageObj.user.name)) {
+                            user = {
+                                name: messageObj.user.name,
+                                nextSocketId: 1,
+                                sockets: {},
+                                subscribers: {},
+                                status: "online"
+                            }
+                            users[messageObj.user.name] = user;
+                        } else {
+                            user = users[messageObj.user.name];
+                        }
+                        socketId = addSocketToUser(socket, user);
+                        statusChange("online", user);
+
+                        // Only using subscription info from the first person to connect
+                        // TODO: if the same person connects more than once, synchronize contacts
+                        if (!user.subscribedToNames) {
+                            console.log("Adding subscribedToNames");
+                            addSubscribedToNames(messageObj.user.subscribedToNames, user, socket);
+                        }
+
+                        user = users[messageObj.user.name];
+                    }
+                    break;
+                case "ChatMessage":
+                    if (messageObj.targetName && users[messageObj.targetName]) {
+                        var targetUser = users[messageObj.targetName];
+                        sendMessageToUser(targetUser, user, messageObj.messageText);
+                    }
+                    else {
+                        console.error("Invalid username: '" + message.targetName +  "' target for a message.");
+                    }
+                    break;
+                default:
+                    console.error("Invalid message id: " + messageObj.messageId);
+                    break;
+            }
+        }
+        else {
+            console.error("Invalid message object.");
+        }
+    });
+
+    socket.on('disconnect', function server$io$on$disconnect() {
+        console.log("Processing disconnect from user: ")
+        console.dir(user);
+        if (user) {
+            removeSocket(user, socketId);
+            user = undefined;
+        }
+    });
+});
---
+++ local_package.csx/ServiceDefinition.rd
@@ -0,0 +1,70 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<serviceModel xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" name="piratechatws" generation="1" functional="0" release="0" Id="0e677cec-9913-46c1-bc28-b32eeea3541e" dslVersion="1.2.0.0" xmlns="http://schemas.microsoft.com/dsltools/RDSM">
+  <groups>
+    <group name="piratechatwsGroup" generation="1" functional="0" release="0">
+      <componentports>
+        <inPort name="WorkerRole1:HttpIn" protocol="tcp">
+          <inToChannel>
+            <lBChannelMoniker name="/piratechatws/piratechatwsGroup/LB:WorkerRole1:HttpIn" />
+          </inToChannel>
+        </inPort>
+      </componentports>
+      <settings>
+        <aCS name="WorkerRole1Instances" defaultValue="[1,1,1]">
+          <maps>
+            <mapMoniker name="/piratechatws/piratechatwsGroup/MapWorkerRole1Instances" />
+          </maps>
+        </aCS>
+      </settings>
+      <channels>
+        <lBChannel name="LB:WorkerRole1:HttpIn">
+          <toPorts>
+            <inPortMoniker name="/piratechatws/piratechatwsGroup/WorkerRole1/HttpIn" />
+          </toPorts>
+        </lBChannel>
+      </channels>
+      <maps>
+        <map name="MapWorkerRole1Instances" kind="Identity">
+          <setting>
+            <sCSPolicyIDMoniker name="/piratechatws/piratechatwsGroup/WorkerRole1Instances" />
+          </setting>
+        </map>
+      </maps>
+      <components>
+        <groupHascomponents>
+          <role name="WorkerRole1" generation="1" functional="0" release="0" software="C:\node\piratechatws\local_package.csx\roles\WorkerRole1" entryPoint="base\x64\WaHostBootstrapper.exe" parameters="base\x64\WaWorkerHost.exe " memIndex="1792" hostingEnvironment="consoleroleadmin" hostingEnvironmentVersion="2">
+            <componentports>
+              <inPort name="HttpIn" protocol="tcp" portRanges="80" />
+            </componentports>
+            <settings>
+              <aCS name="__ModelData" defaultValue="&lt;m role=&quot;WorkerRole1&quot; xmlns=&quot;urn:azure:m:v1&quot;&gt;&lt;r name=&quot;WorkerRole1&quot;&gt;&lt;e name=&quot;HttpIn&quot; /&gt;&lt;/r&gt;&lt;/m&gt;" />
+            </settings>
+            <resourcereferences>
+              <resourceReference name="DiagnosticStore" defaultAmount="[4096,4096,4096]" defaultSticky="true" kind="Directory" />
+              <resourceReference name="EventStore" defaultAmount="[1000,1000,1000]" defaultSticky="false" kind="LogStore" />
+            </resourcereferences>
+          </role>
+          <sCSPolicy>
+            <sCSPolicyIDMoniker name="/piratechatws/piratechatwsGroup/WorkerRole1Instances" />
+            <sCSPolicyFaultDomainMoniker name="/piratechatws/piratechatwsGroup/WorkerRole1FaultDomains" />
+          </sCSPolicy>
+        </groupHascomponents>
+      </components>
+      <sCSPolicy>
+        <sCSPolicyFaultDomain name="WorkerRole1FaultDomains" defaultPolicy="[2,2,2]" />
+        <sCSPolicyID name="WorkerRole1Instances" defaultPolicy="[1,1,1]" />
+      </sCSPolicy>
+    </group>
+  </groups>
+  <implements>
+    <implementation Id="9fcfcd9f-81ba-4701-9296-915161b26f69" ref="Microsoft.RedDog.Contract\ServiceContract\piratechatwsContract@ServiceDefinition">
+      <interfacereferences>
+        <interfaceReference Id="72451e5f-1429-49cd-99ab-712f3bcb1bd7" ref="Microsoft.RedDog.Contract\Interface\WorkerRole1:HttpIn@ServiceDefinition">
+          <inPort>
+            <inPortMoniker name="/piratechatws/piratechatwsGroup/WorkerRole1:HttpIn" />
+          </inPort>
+        </interfaceReference>
+      </interfacereferences>
+    </implementation>
+  </implements>
+</serviceModel>
